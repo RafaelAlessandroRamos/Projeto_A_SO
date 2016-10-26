e7e79414075e1b5645df7e5e6b5968f2b3468d57
Rafael <Rafael@DESKTOP-Q9HD1FA>
2016-10-26 16:20:39 -0200

    Inicio do projeto: criação das classes processador, manipulacao_Arquivo, processo, escalonador.

---
+++ src/Main/Processo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Processo {
+    private int id, fase, prioridade, tempoChegada, pc;
+    private char tipo; // Usuario ou sistema
+    private List<Object> FilaEntradaSaida;
+    
+}
