0445e3c9bb8bdd7fa087c8b23f182cb354f65362
Rafael <Rafael@DESKTOP-Q9HD1FA>
2016-10-27 21:52:30 -0200

    Inicio do projeto: classe processo, classe manipulaArquivo terminada.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,6 +6,7 @@
 package Main;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  *
@@ -17,7 +18,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
+
+        String caminho = "Dados_Entrada.txt";
+
+        List<String> dadosArquivo = manipulaArquivo.abrirArquivo(caminho); // armazena todas as linhas do arquivo
+
+        //imprime a lista da string de dados do arquivo
+        String[] string;
+        for (String s : dadosArquivo) {
+            string = s.split(" ");
+            System.out.println(string[1]+"\t"+string[2]+"\t"+string[3]+"\t"+string[4]+"\n");
+            //Processo processo = new Processo(Integer.parseInt(string[0]), Integer.parseInt(string[1]), Integer.parseInt(string[2]), Integer.parseInt(string[3]));
+            //processo.toString();
+        }
+
     }
-    
+
 }
