61457a5b0737b599527eaca5088349f336be64fa
Rafael <Rafael@DESKTOP-Q9HD1FA>
2016-12-04 13:41:58 -0200

    finalizado.

--- src/Main/SJF.java
+++ src/Main/SJF.java
@@ -20,7 +20,7 @@ public class SJF {
         executar();
     }
 
-    public void verificaTempo(int tempoAtual) {
+    public void verificarTempo(int tempoAtual) {
         for (int i = 0; i < controlaListas.getListaProcessos().size(); i++) {
             if (tempoAtual == controlaListas.getListaProcessos().get(i).getTempoChegada()) { // verifica se o tempo atual na linha de tempo é igual ao tempo de chegada de algum processo na lista de processos
                 controlaListas.addFilaProntos(controlaListas.getListaProcessos().get(i)); // Adiciona um processo na lista de prontos
@@ -49,7 +49,7 @@ public class SJF {
             if (controlaListas.getListaProcessos().isEmpty() && controlaListas.getFilaBloqueados().isEmpty() && controlaListas.getFilaProntos().isEmpty() && controlaListas.getExecutando() == null) { // condição de parada: se tudo estiver vazio para o laço
                 break;
             }
-            verificaTempo(this.tempoAtual);
+            verificarTempo(this.tempoAtual);
             if (!controlaListas.getFilaProntos().isEmpty()) { // se a fila de pronto nao estiver vazia
                 controlaListas.quickSort(controlaListas.getFilaProntos(), 0, controlaListas.getFilaProntos().size() - 1, 1); // ordena a lista de prontos por prioridade
                 controlaListas.setExecutando(controlaListas.getFilaProntos().getFirst()); // pega o primeiro processo da lista de pronto para executar
@@ -91,7 +91,7 @@ public class SJF {
             } else if (processo.getFilaEntradaSaida().get(processo.getPc()) == 1) { // Se na posição pc estiver 0, pc + 1
                 processo.setPc(processo.getPc() + 1); // pc+1 no processo
                 processo.setTempoPreempcao(tempoAtual);
-                bloqueado();
+                bloquear();
                 controlaListas.setExecutando(null);
             }
             controlaListas.addFilaProntos(processo);
@@ -108,7 +108,7 @@ public class SJF {
         controlaListas.getFilaBloqueados().removeFirst();
     }
 
-    private void bloqueado() {
+    private void bloquear() {
         controlaListas.addFilaBloqueados(controlaListas.getExecutando());
         controlaListas.setExecutando(null);
     }
